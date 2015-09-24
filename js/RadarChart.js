27ddd386268adf4e352ca340e9100fb943948d06
Mukhlis Jamato <mukhlis.jamato@gmail.com>
2015-09-24 15:07:24 -0600

    revamped 70%

--- js/RadarChart.js
+++ js/RadarChart.js
@@ -1,18 +1,9 @@
-﻿var data = {
-    labels: ["JS", "JS Frameworks", "Ruby", "HTML", "C++", "Python", "PHP"],
+﻿var data1 = {
+    labels: ["JS", "JS Frameworks*", "Ruby", "HTML", "C++", "Python", "PHP"],
     datasets: [
+        
         {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 90, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
+            label: "Web Dev Languages",
             fillColor: "rgba(151,187,205,0.2)",
             strokeColor: "rgba(151,187,205,1)",
             pointColor: "rgba(151,187,205,1)",
@@ -24,4 +15,23 @@
     ]
 };
 
-var myLineChart = new Chart(document.getElementById("canvas").getContext("2d")).Radar(data, { scaleFontSize: 13, scaleFontColor: "#ffa45e", pointDot: false, pointLabelFontColor: "#fff", responsive: true });
+var WebChart = new Chart(document.getElementById("canvasWeb").getContext("2d")).Radar(data1, { scaleFontSize: 13, scaleFontColor: "#ffa45e", pointDot: false, pointLabelFontColor: "#333", responsive: true });
+
+var data2 = {
+    labels: ["ASPEN Tech*", "MATLAB", "3D Cad", "Cost Optimization", "MS Visio", "Data Infographics", "Economic Analysis"],
+    /*labels: ["Process and Equipment Design", "Process and Equipment Simulation", "Numerical Data Analysis", "Cost Optimization", "Computational Mathematical Modelling", "Safety and Quality", "Project Management"],*/
+    datasets: [
+        {
+            label: "Chemical Engineering Skills",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 90, 81, 56, 55, 40]
+        },
+    ]
+};
+
+var EngChart = new Chart(document.getElementById("canvasEng").getContext("2d")).Radar(data2, { scaleFontSize: 13, scaleFontColor: "#ffa45e", pointDot: false, pointLabelFontColor: "#333", responsive: true });
